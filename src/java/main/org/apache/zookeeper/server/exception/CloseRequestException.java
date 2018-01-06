b524025df66c3dc53db09921e911b0f9edabeada
bitgaoshu <bitgaoshu@gmail.com>
2018-01-06 23:00:30 +0800

    refactor server ZKException.java

---
+++ src/java/main/org/apache/zookeeper/server/exception/CloseRequestException.java
@@ -0,0 +1,11 @@
+package org.apache.zookeeper.server.exception;
+
+import java.io.IOException;
+
+public class CloseRequestException extends IOException {
+    private static final long serialVersionUID = -7854505709816442681L;
+
+    public CloseRequestException(String msg) {
+        super(msg);
+    }
+}
